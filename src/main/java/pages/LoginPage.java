aa81029eea44a2ca6d6e3626b7970a2cdc112ec2
RAKESH13595 <47477434+RAKESH13595@users.noreply.github.com>
2025-03-11 12:48:09 +0530

    Optimize loginPage

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,35 +1,41 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import utils.Log;
 
 public class LoginPage {
 	private WebDriver driver;
 	
-	private By usernameTextBox = By.id("Email");
-	private By passwordTextBox = By.id("Password");
-	private By loginButton = By.xpath("//*[@id=\"main\"]/div/div/div/div[2]/div[1]/div/form/div[3]/button");
+	@FindBy(id="Email")
+	WebElement usernameTextbox;
+	@FindBy(id="Password")
+	WebElement passwordTextbox;
+	@FindBy(xpath="//*[@id=\"main\"]/div/div/div/div[2]/div[1]/div/form/div[3]/button")
+	WebElement loginButton;
 	
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
+		PageFactory.initElements(driver,this);
 	}
 	
 	public void enterUsername(String username) {
 		Log.info("Entering Username : " + username);
-		driver.findElement(usernameTextBox).clear();
-		driver.findElement(usernameTextBox).sendKeys(username);
+		usernameTextbox.clear();
+		usernameTextbox.sendKeys(username);
 	}
 	
 	public void enterPassword(String password) {
 		Log.info("Entering password");
-		driver.findElement(passwordTextBox).clear();
-		driver.findElement(passwordTextBox).sendKeys(password);
+		passwordTextbox.clear();
+		passwordTextbox.sendKeys(password);
 	}
 	
 	public void clickLogin() {
 		Log.info("Clicking Login button");
-		driver.findElement(loginButton).click();
+		loginButton.click();
 	}
 }
